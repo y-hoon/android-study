2570d56a048fa49144aff0a69cc934d13849d2cb
YOUNGHOON PARK <digitcomgisul@gmail.com>
2021-06-10 23:01:19 +0900

    변수 자료형 연습
    ? null 허용 변수
    $ 스트링 치환

---
+++ app/src/main/java/com/example/myapplication/kotlin/03.Practice.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.kotlin
+
+var b = "1"
+var c = b.toInt()
+var d = b.toFloat()
+
+var e = "John"
+var f = "My name is $e Nice to meet you"
+
+var abc : Int? = null
+
+fun main(array: Array<String>) {
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+
+}
